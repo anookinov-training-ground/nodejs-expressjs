6252521f9d4debbcca8d72f1b8da2d34cf15e3aa
anookinov <anookinov@gmail.com>
2021-05-21 10:22:21 +0700

    add async requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -208,8 +208,9 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  // const prodId = req.body.productId;
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -220,12 +221,14 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log('DELETED PRODUCT');
-      res.redirect('/admin/products');
+      // res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success!' });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      // const error = new Error(err);
+      // error.httpStatusCode = 500;
+      // return next(error);
+      res.status(500).json({ message: 'Deleting product failed.' });
     });
   // .catch(err => next(err))
   // Product.findByIdAndRemove(prodId)
